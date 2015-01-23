3d5a7fdc19a7e47709140cff70a84c8510bb354d
AndrewNeo <andrew@andrewneo.com>
2015-01-23 00:08:58 -0800

    Add a day-list selector to the link list

--- src/js/src/links.js
+++ src/js/src/links.js
@@ -8,30 +8,41 @@ function optionsLoaded(obj) {
 
 function bindPage() {
     jQuery("#open_in_tabs").click(openInTabs);
+    jQuery("#daylist").change(dayChange);
 }
 
+var all_links;
 var links_list;
 
 function loadLinks(links) {
+    all_links = links;
+
+    var dow = (new Date()).getDay();
+    jQuery("#daylist option").eq(dow + 1).prop("selected", true);
+    loadLinkDay(dow);
+}
+
+function loadLinkDay(dow) {   
     var list_container = jQuery("#link_list");
 
-    if (!links || links.length == 0) {
+    if (!all_links || all_links.length == 0) {
         list_container.text("No links are configured.");
         return;
     }
 
-    var dow = (new Date()).getDay();
     links_list = new Array();
-
-    var today_links = links.map(function(f) {
+    var today_links = all_links.map(function(f) {
         return [intToByteArray(f[0]), f[1], f[2]];
     }).filter(function(f) {
-        return f[0][7] && f[0][dow];
+        return f[0][7] && (dow == -1 || f[0][dow]);
     });
 
     if (today_links && today_links.length > 0) {
         list_container.empty();
         jQuery("#open_in_tabs").show();
+    } else {
+        list_container.text("Nothing today!");
+        jQuery("#open_in_tabs").hide();
     }
 
     jQuery.each(today_links, function(i, item) {
@@ -39,6 +50,11 @@ function loadLinks(links) {
         jQuery("<br>").appendTo(list_container);
         links_list.push(item[2]);
     });
+}
+
+function dayChange(e) {
+    var index = $("#daylist option:selected").index();
+    loadLinkDay(index - 1);
 }
 
 function openInTabs() {
